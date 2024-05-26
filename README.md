45ee2346562947b08b8f9d325adaf20a9ccf9e38
Hirako San <hirako2000@gmail.com>
2024-05-26 13:27:22 +0700

    👾 init

---
+++ README.md
@@ -0,0 +1,12 @@
+### f7k counter
+
+Just a landing page counter
+
+### Commands
+
+```bash
+npm install # clone the repo first and from root, node18 works
+npm run start # for dev mode
+npm run build # to build the production assets
+npm run lint # to lint all code
+```
---
+++ src/script.ts
@@ -0,0 +1,50 @@
+
+const init = (): void => {
+
+    let clock;
+
+    // Grab the current date
+    const currentDate = new Date();
+
+    // Target future date (local time in Australia/Sydney)
+    const targetDate = new Date("2024-09-01T00:00:00+10:00").getTime();
+
+    // Calculate the difference in seconds between the future and current date
+    const diff = Math.floor((targetDate - currentDate.getTime()) / 1000);
+
+    if (diff <= 0) {
+        // If remaining countdown is 0
+        clock = new FlipClock(document.querySelector(".clock"), 0, {
+            clockFace: "DailyCounter",
+            countdown: true,
+            autoStart: false
+        });
+        console.log("Date has already passed!");
+    } else {
+        // Run countdown timer
+        clock = new FlipClock(document.querySelector(".clock"), diff, {
+            clockFace: "DailyCounter",
+            countdown: true,
+            autoStart: true,
+            callbacks: {
+                stop: () => {
+                    console.log("Timer has ended!");
+                }
+            }
+        });
+
+        // Check when timer reaches 0, then stop at 0
+        const checkTime = () => {
+            const t = clock.time;
+            if (t <= 0) {
+                clock.time =0;
+            } else {
+                setTimeout(checkTime, 1000);
+            }
+        };
+
+        setTimeout(checkTime, 1000);
+    }
+};
+
+init();
